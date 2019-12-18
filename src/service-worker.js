a9a4350b7e9a5e4c083947b793e97e44c0c27ba6
Flavio Bravo <f980534860@gmail.com>
2019-12-18 16:45:30 -0500

    adding cli-plugin-pwa and trying workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,6 +10,37 @@ workbox.core.setCacheNameDetails({prefix: "quote-app"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+self.addEventListener('install', e => {
+
+  workbox.routing.registerRoute(
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'APP_SHELL',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    'service-worker.js',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'APP_SHELL',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    'img/icons/favicon-32x32.png',
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'APP_SHELL',
+    })
+  );
+
+});
+
+/*self.addEventListener( 'fetch', e => {
+
+  console.log('Event Fetch: ',e.request.url);
+
+});*/
+
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
       self.skipWaiting();
