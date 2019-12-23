4eff9ce6772cee628dc4487106b5c29a4fe8028c
Flavio Bravo <f980534860@gmail.com>
2019-12-23 15:28:59 -0500

    adding express - MEVN App

---
+++ server/notes.js
@@ -0,0 +1,41 @@
+// Routes.js - Módulo de rutas
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+// Get notes
+router.get('/', function (req, res) {
+  
+  const notes = require('./subs-db.json');
+  console.log('GET => ',notes);
+  res.json({
+      ok: true,
+      respond: notes
+  });
+
+});
+
+
+// Post one note
+router.post('/', function (req, res) {
+  
+  let notes = require('./subs-db.json');
+
+  const id = notes.length + 1;
+  const note = {
+    _id: `${ id }`,
+    description: req.body.description
+  };
+
+  notes.push( note );
+  console.log("POST => ", note);
+
+  fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(notes) );
+
+  res.json({
+    ok: true,
+    respond: note
+  });
+});
+
+module.exports = router;
